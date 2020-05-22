0eb6e198a2ecf77ff1b7d0ee757121f8e37c2e2f
Ella Gomez <elviagomez133@gmail.com>
2020-05-22 17:50:38 +0200

    modal finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,11 @@
 import '../styles/styles.css';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
+
+new Modal()
+
+let stickyHeader = new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
